b26682e2186215c2a0e2ffb9b92affdda0732bfb
Mwin456 <mwinplay03@gmail.com>
2025-06-21 16:25:27 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MWinPlayApp());
+
+class MWinPlayApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MWinPlay',
+      home: Scaffold(
+        appBar: AppBar(title: Text('MWinPlay')),
+        body: Center(child: Text('Welcome to MWinPlay Game')),
+      ),
+    );
+  }
+}
